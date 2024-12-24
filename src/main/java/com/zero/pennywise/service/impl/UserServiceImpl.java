7a7f2d22e036e2174ed8aaeb966768dcacf626ed
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-12-24 18:12:31 +0900

    Refactor : 코드 수정

--- src/main/java/com/zero/pennywise/service/impl/UserServiceImpl.java
+++ src/main/java/com/zero/pennywise/service/impl/UserServiceImpl.java
@@ -23,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+
   private final UserHandler userHandler;
   private final JwtUtil jwtUtil;
 
