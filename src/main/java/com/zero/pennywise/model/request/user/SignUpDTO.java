5a00637dc28048ea173f7ef16e6ea5cc4b49aef0
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-12-30 14:35:46 +0900

    Refactor : 클래스명 변경 및 static factory method 도입

--- src/main/java/com/zero/pennywise/model/request/user/SignUpDTO.java
+++ src/main/java/com/zero/pennywise/model/request/user/SignUpDTO.java
@@ -1,5 +1,7 @@
 package com.zero.pennywise.model.request.user;
 
+import com.zero.pennywise.entity.UserEntity;
+import com.zero.pennywise.model.type.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -11,7 +13,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class RegisterDTO {
+public class SignUpDTO {
 
   @NotEmpty(message = "아이디를 입력해주세요.")
   @Email(message = "이메일 형식이 아닙니다.")
@@ -22,5 +24,14 @@ public class RegisterDTO {
 
   @NotEmpty(message = "닉네임을 입력해주세요.")
   private String nickname;
+
+  public static UserEntity of (SignUpDTO signUpDTO, String password) {
+    return UserEntity.builder()
+        .email(signUpDTO.getEmail())
+        .password(password)
+        .nickname(signUpDTO.getNickname())
+        .role(UserRole.USER)
+        .build();
+  }
 
 }
