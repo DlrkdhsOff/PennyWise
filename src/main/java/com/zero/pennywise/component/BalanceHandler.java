ec85ce394137cb7e0398394ef14a4fda012e89b3
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-12-31 18:10:23 +0900

    Refactor : 메시지 전달 방식 메서드 추가

--- src/main/java/com/zero/pennywise/component/BalanceHandler.java
+++ src/main/java/com/zero/pennywise/component/BalanceHandler.java
@@ -4,10 +4,15 @@ import com.zero.pennywise.entity.BalanceEntity;
 import com.zero.pennywise.entity.CategoryEntity;
 import com.zero.pennywise.entity.TransactionEntity;
 import com.zero.pennywise.entity.UserEntity;
+import com.zero.pennywise.entity.WaringMessageEntity;
+import com.zero.pennywise.model.response.waring.WaringMessageDTO;
 import com.zero.pennywise.repository.BalanceRepository;
+import com.zero.pennywise.repository.WaringMessageRepository;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,6 +20,8 @@ import org.springframework.stereotype.Component;
 public class BalanceHandler {
 
   private final BalanceRepository balanceRepository;
+  private final WaringMessageRepository waringMessageRepository;
+  private final RedisTemplate<String, Object> redisTemplate;
   private final RedisHandler redisHandler;
 
   // 거래 생성 또는 추가
@@ -22,6 +29,18 @@ public class BalanceHandler {
     String recordMonth = getRecordMonth();
     BalanceEntity balance = getOrCreateBalance(user, transaction.getCategory(), recordMonth);
     addTransactionAmount(balance, transaction.getAmount(), transaction.getType().isExpenses());
+
+    if (balance.getTotalExpensesAmount() < 0) {
+      String message = transaction.getCategory().getCategoryName() + " 예산을 초과 했습니다.";
+      WaringMessageEntity warningMessage = WaringMessageEntity.builder()
+          .user(user)
+          .message(message)
+          .recivedDateTime(LocalDateTime.now())
+          .build();
+
+      waringMessageRepository.save(warningMessage);
+      redisTemplate.convertAndSend("notifications", new WaringMessageDTO(user.getUserId(), message));
+    }
     saveBalance(balance);
     redisHandler.updateBalance(user, balance);
   }
