14b4bbf2e17ca30264f8209db674eb96b910feeb
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-01-03 18:55:32 +0900

    Refactor : 사용하지 않은 import 구문 제거

--- src/test/java/com/zero/pennywise/component/facade/FacadeManagerTest.java
+++ src/test/java/com/zero/pennywise/component/facade/FacadeManagerTest.java
@@ -43,7 +43,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
