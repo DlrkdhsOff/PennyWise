7c9a9568100896d4c75ec4230a653f5d3c447976
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-01-06 16:33:03 +0900

    Test : 의존성 주입

--- src/test/java/com/zero/pennywise/service/impl/CategoryServiceImplTest.java
+++ src/test/java/com/zero/pennywise/service/impl/CategoryServiceImplTest.java
@@ -1,7 +1,15 @@
 package com.zero.pennywise.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.zero.pennywise.component.facade.FacadeManager;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class CategoryServiceImplTest {
+
+  @Mock
+  private FacadeManager facadeManager;
+
 
 }
