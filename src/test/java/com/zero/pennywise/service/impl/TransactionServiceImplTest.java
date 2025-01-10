23e9d374a675f03df7011036a7a7af0f69ad8a68
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2025-01-10 19:35:19 +0900

    Test : TransactionServiceImpl 테스트 코드 작성

--- src/test/java/com/zero/pennywise/service/impl/TransactionServiceImplTest.java
+++ src/test/java/com/zero/pennywise/service/impl/TransactionServiceImplTest.java
@@ -1,12 +1,39 @@
 package com.zero.pennywise.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import com.zero.pennywise.component.facade.FacadeManager;
+import com.zero.pennywise.model.response.page.PageResponse;
+import com.zero.pennywise.model.response.transaction.Transactions;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
 @ExtendWith(MockitoExtension.class)
 class TransactionServiceImplTest {
+
+  @Mock
+  private FacadeManager facadeManager;
+
+  @Mock
+  private HttpServletRequest request;
+
+  @InjectMocks
+  private TransactionServiceImpl transactionService;
+
+  private final int PAGE = 1;
+
+  private PageResponse<Transactions> pageResponse;
+
+  @BeforeEach
+  void setUp() {
+    List<Transactions> transactionsList = List.of(
+        new Transactions(1L, "지출", "카테고리1", 1000L, "상세 설명 1", "시간1"),
+        new Transactions(2L, "지출", "카테고리1", 2000L, "상세 설명 2", "시간2")
+    );
+  }
 
 }
